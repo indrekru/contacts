0daff5d8252d17b458bcd99433d948ee6f0392b9
Indrek <indrekruubel@gmail.com>
2023-02-26 11:18:21 +0200

    dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
+import SimpleCrypto from "simple-crypto-js"
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
 
+    const [secret, setSecret] = useState('secret')
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [newContact, setNewContact] = useState(false);
 
     function loadContacts() {
         setLoading(true);
+        setNewContact(false);
         fetch('http://localhost:8080/api/v1/contacts')
         .then(response => {
             if (response.ok) {
@@ -23,15 +26,24 @@ export default function App() {
         .finally(() => setLoading(false));
     }
 
+    function createNewContact() {
+        setData([]);
+        setNewContact(true);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Kontaktid
+          Kontaktid (key: {secret})
         </p>
         <button onClick={loadContacts}>
           Lae kontaktid
         </button>
+        <button onClick={createNewContact}>
+            Uus kontakt
+        </button>
+        {/* List */}
         {loading && <div>Loading...</div>}
         {data && data.map((item) =>
           <div key={item.id}>
@@ -42,6 +54,20 @@ export default function App() {
             <div>-----------------------------</div>
           </div>
         )}
+        {/* List end */}
+
+        {/* New contact */}
+        {newContact &&
+            <div>
+                <label>Name:</label>
+                <input type="text" />
+                <label>Code name:</label>
+                <input type="text" />
+                <label>Phone:</label>
+                <input type="text" />
+            </div>
+        }
+        {/* New contact end */}
       </header>
     </div>
   );
